eaa3accda5499b3c83e62992a1f2ba62a47e092b
root <root@Rushab>
2025-01-12 11:37:49 +0545

    initialize login

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,15 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-import path from "path" 
+
 export default {
-  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}', // Adjust the paths according to your project structure
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+
 }
 
