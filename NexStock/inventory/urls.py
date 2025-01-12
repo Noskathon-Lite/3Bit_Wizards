756f2288d6384fdc46a804181cf4410440cca093
prawin <yadavprawin126@gmail.com>
2025-01-13 00:46:15 +0545

    trying

--- NexStock/inventory/urls.py
+++ NexStock/inventory/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('pages-contact/', views.contact, name='contact'),
+    path('forms-layouts/', views.forms_layouts, name='forms_layouts'),
+    path('tables-data/', views.tables_data, name='tables_data'),
+    path('charts-chartjs/', views.charts_chartjs, name='charts_chartjs'),
+    path('users-profile/', views.users_profile, name='users_profile'),
+    path('pages-register/', views.pages_register, name='pages_register'),
+    path('pages-login/', views.pages_login, name='pages_login'),
+    path('api/update-element/', views.update_element, name='update_element'),
+    path('api/get-element/<int:element_id>/', views.get_element, name='get_element'),
+    path('pages-contact/tables-data.html',views.tables_data),
 ]
